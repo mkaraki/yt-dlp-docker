f35c931afff7a915d7eccff693a6ede7dd0a6278
mkaraki <26180919+mkaraki@users.noreply.github.com>
2024-01-04 03:18:25 +0900

    Add exit code validation

--- entry.sh
+++ entry.sh
@@ -5,4 +5,13 @@ apt-get update > /dev/null
 apt-get upgrade -y > /dev/null
 pip install --upgrade pip yt-dlp > /dev/null
 
+if [[ "$1" == "yt-dlp" ]] ; then
+  shift 1
+fi
+
 yt-dlp $*
+ytdlp_exit_code=$?
+
+if [[ $ytdlp_exit_code -ne 0 ]] ; then
+    exit $ytdlp_exit_code
+fi
